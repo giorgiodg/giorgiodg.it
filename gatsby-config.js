6491527e8389ceae6fd69d2a01ac9183832af110
Giorgio Delle Grottaglie <giorgiodg@gmail.com>
2024-01-05 21:34:01 +0100

    Removed tracking and related banner. Used yarn to get a successful build

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,39 +26,6 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     {
-      resolve: `gatsby-plugin-cookiehub-banner`,
-      options: {
-        cookieHubId: "97f1816c",
-        cookieHubV2Api: true,
-        categories: [
-          {
-            categoryName: "analytics", // Unique id of the category which is set by Cookiehub.
-            cookieName: "gatsby-plugin-google-analytics-gdpr_cookies-enabled", // Your custom cookie name
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics-gdpr`,
-      options: {
-        // The property ID; the tracking code won't be generated without it.
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
-        // Optional parameter (default false) - Enable analytics in development mode.
-        enableDevelopment: false, // default false
-        // Optional parameter (default true) - Some countries (such as Germany) require you to use the _anonymizeIP function for Google Analytics. Otherwise you are not allowed to use it.
-        anonymizeIP: true,
-        // Optional parameter (default false) - Starts google analytics with cookies enabled. In some countries (such as Germany) this is not allowed.
-        autoStartWithCookiesEnabled: false,
-        // Optional parameter - Configuration for react-ga and google analytics
-        reactGaOptions: {
-          debug: true,
-          gaOptions: {
-            sampleRate: 100,
-          },
-        },
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: siteTitle,
