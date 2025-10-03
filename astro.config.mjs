f0bd01fa08e609c28057c71da59f6fefd8157f3f
Giorgio Delle Grottaglie <giorgiodg@gmail.com>
2025-10-03 09:24:16 +0200

    Unindexed cv page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://giorgiodg.it",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => page !== "https://giorgiodg.it/cv/",
+    }),
+    tailwind(),
+  ],
 });
